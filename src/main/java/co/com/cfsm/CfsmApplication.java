ee7922d6e1e2d4f7942f7a9d5aaca7e5d28a8aef
Cristian Sepulveda <cristian11611@gmail.com>
2026-06-21 13:25:49 -0500

    test

---
+++ src/main/java/co/com/cfsm/CfsmApplication.java
@@ -0,0 +1,13 @@
+package co.com.cfsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfsmApplication.class, args);
+	}
+
+}
